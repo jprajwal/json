efaa1581f954074ce4ec4bd3a9d130c3ef080363
jprajwal <prajwaltj@gmail.com>
2022-11-23 00:17:50 +0530

    Refactored utf16 codec to remove over engineering

--- include/codec/utf16codec.h
+++ include/codec/utf16codec.h
@@ -1,37 +1,35 @@
 #ifndef JSON_CODEC_UTF16CODEC_H
 #define JSON_CODEC_UTF16CODEC_H
 
-#include "ichar.h"
-#include "icodec.h"
+#include "rune.h"
+#include "utf16char.h"
 
 #include <cstdint>
-#include <stdexcept>
 #include <string>
 
 namespace json {
 namespace codec {
+namespace utf16 {
 
-class UTF16Codec : public CodecTraitBase<char16_t> {
-public:
-  ICharBase<char16_t> encodeSingle(Rune rune) {
-    if (!rune.isValid()) {
-      throw std::runtime_error{std::string{"UTF16CharEncodeError"}};
-    }
-    if (rune.isBMP()) {
-      ICharBase<char16_t> result(1);
-      result.at(0) = static_cast<char16_t>(static_cast<std::uint32_t>(rune));
-      return result;
-    }
-    const std::uint16_t temp{static_cast<std::uint16_t>(rune - 0x10000)};
-    const std::uint16_t hs{static_cast<std::uint16_t>((temp >> 10) + 0xD800)};
-    const std::uint16_t ls{static_cast<std::uint16_t>((temp & 0x3FF) + 0xDC00)};
-    ICharBase<char16_t> ch(2);
-    ch.at(0) = static_cast<char16_t>(hs);
-    ch.at(1) = static_cast<char16_t>(ls);
+UTF16Char encodeOne(Rune rune) {
+  if (!rune.isValid()) {
+    throw rune;
+  }
+  if (rune.isBMP()) {
+    UTF16Char ch(1);
+    ch.at(0) = static_cast<char16_t>(static_cast<std::uint16_t>(rune));
     return ch;
   }
-};
+  const std::uint16_t temp{static_cast<std::uint16_t>(rune - 0x10000)};
+  const std::uint16_t hs{static_cast<std::uint16_t>((temp >> 10) + 0xD800)};
+  const std::uint16_t ls{static_cast<std::uint16_t>((temp & 0x3FF) + 0xDC00)};
+  UTF16Char ch(2);
+  ch.at(0) = static_cast<char16_t>(hs);
+  ch.at(1) = static_cast<char16_t>(ls);
+  return ch;
+}
 
+} // namespace utf16
 } // namespace codec
 } // namespace json
 
--- tests/codec/test_utf16codec.cpp
+++ tests/codec/test_utf16codec.cpp
@@ -3,10 +3,11 @@
 #include "test.h"
 #include <cassert>
 
+namespace utf16 = json::codec::utf16;
+
 void testUTF16SimpleSurrogatePairEncode() {
-  json::codec::UTF16Codec codec{};
   json::codec::Rune rune{0x10437};
-  auto result{codec.encodeSingle(rune)};
+  auto result{utf16::encodeOne(rune)};
   json::log << std::hex << "hs: " << result.at(0) << std::endl;
   json::log << std::hex << "ls: " << result.at(1) << std::endl;
   assert(result.at(0) == 0xD801);
