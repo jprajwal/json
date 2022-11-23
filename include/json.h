af2a8b6bf6bf7c8e9d301035a0d4972c4d9b6d6f
jprajwal <prajwaltj@gmail.com>
2022-11-24 01:43:36 +0530

    More operations implemented for json string

--- include/json.h
+++ include/json.h
@@ -3,7 +3,9 @@
 
 #include "null.h"
 
+#include <iostream>
 #include <map>
+#include <stdexcept>
 #include <string>
 #include <utility>
 
@@ -16,12 +18,12 @@ enum class Type {
 };
 
 class Json {
-
 public:
   Json() : mNull{} {}
-  Json(const char *str) : mStr{str} {}
-  Json(const std::string &str) : mStr{str} {}
-  Json(std::string &&str) : mStr{std::move(str)} {}
+  Json(const char *str) : mType{Type::String}, mStr{str} {}
+  Json(const std::string &str) : mType{Type::String}, mStr{str} {}
+  Json(std::string &&str) : mType{Type::String}, mStr{std::move(str)} {}
+
   ~Json() {
     switch (mType) {
     case Type::String:
@@ -32,6 +34,52 @@ public:
     case Type::Object:
       break;
     }
+  }
+
+  Type type() { return mType; }
+
+  bool operator==(const Json &other) {
+    if (mType != other.mType) {
+      return false;
+    }
+    switch (mType) {
+    case Type::String:
+      return mStr == other.mStr;
+    default:
+      return false;
+    }
+  }
+
+  using ConstStringIterator = std::string::const_iterator;
+
+  ConstStringIterator stringCBegin() {
+    if (mType != Type::String) {
+      throw std::runtime_error{
+          "stringBegin() is supported only for json strings"};
+    }
+    return mStr.cbegin();
+  }
+
+  ConstStringIterator stringCEnd() {
+    if (mType != Type::String) {
+      throw std::runtime_error{
+          "stringEnd() is supported only for json strings"};
+    }
+    return mStr.cend();
+  }
+
+  friend std::ostream &operator<<(std::ostream &out, const Json &js) {
+    switch (js.mType) {
+    case Type::String:
+      out << js.mStr;
+      break;
+    case Type::Null:
+      out << "null";
+      break;
+    default:
+      out << "Unimplemented";
+    }
+    return out;
   }
 
 private:
--- tests/test_json.cpp
+++ tests/test_json.cpp
@@ -41,19 +41,50 @@ int main() {
 */
 
 #include "json.h"
+#include "test.h"
+
+#include <cassert>
+#include <iostream>
+#include <sstream>
 #include <string>
 
-void testDefaultCstr() { json::Json j; }
+void testDefaultCstr() {
+  json::Json j;
+  assert(j.type() == json::Type::Null);
+}
 
 void testJsonStrCopyCstr() {
   std::string s{"test"};
   json::Json j{s};
+  assert(j.type() == json::Type::String);
 }
 
-void testJsonStrMoveCstr() { json::Json j{"test"}; }
+void testJsonStrMoveCstr() {
+  json::Json j{"test"};
+  assert(j.type() == json::Type::String);
+}
+
+void testJsonConstStringIterator() {
+  json::Json j{"test"};
+  for (auto ch{j.stringCBegin()}; ch != j.stringCEnd(); ++ch) {
+    json::log << *ch << ' ';
+  }
+  json::log << std::endl;
+  assert(j == "test");
+}
+
+void testJsonStringOstreamInsertion() {
+  json::Json j{"test"};
+  std::stringstream ss;
+  ss << j;
+  json::log << ss.str();
+  assert(ss.str() == "test");
+}
 
 int main() {
   testDefaultCstr();
   testJsonStrCopyCstr();
   testJsonStrMoveCstr();
+  testJsonConstStringIterator();
+  testJsonStringOstreamInsertion();
 }
